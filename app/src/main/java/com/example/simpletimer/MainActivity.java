f4a50d682aafe1e3be9647ef6ff4b3a51abb3cb0
Jacob Nash <nashjr@g.cofc.edu>
2021-01-28 18:39:06 -0500

    lab done?

--- app/src/main/java/com/example/simpletimer/MainActivity.java
+++ app/src/main/java/com/example/simpletimer/MainActivity.java
@@ -3,13 +3,51 @@ package com.example.simpletimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    public class CountDownTimer {
+    private MyCountDownTimer CountDownTimer;
+    private long timeElapsed;
+    private boolean timerHasStarted = false;
+    private Button startB;
+    private TextView text;
+    private TextView timeElapsedView;
+    private final long startTime = 50 * 1000;
+    private final long interval = 1 * 1000;
 
-        public CountDownTimer() {
+    @Override
+    public void onClick(View view) {
+        if(!timerHasStarted) {
+            CountDownTimer.start();
+            timerHasStarted = true;
+            startB.setText(R.string.startLabel);
+        } else {
+            CountDownTimer.cancel();
+            timerHasStarted = false;
+            startB.setText("Reset");
+        }
+    }
+
+    class MyCountDownTimer extends android.os.CountDownTimer {
+
+        public MyCountDownTimer(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        @Override
+        public void onTick(long l) {
+            text.setText("Time remain: " + millisUntilFinished);
+            timeElapsed = startTime - millisUntilFinished;
+            timeElapsedView.setText("Time Elapsed: " + String.valueOf(timeElapsed));
+        }
+
+        @Override
+        public void onFinish() {
+            text.setText("Time's up!");
+            timeElapsedView.setText("Time Elapsed: " + String.valueOf(startTime));
         }
     }
 
@@ -17,10 +55,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public void onClick(View view) {
-
+        startB = (Button) this.findViewById(R.id.button);
+        startB.setOnClickListener(this);
+        text = (TextView) this.findViewById(R.id.timer);
+        timeElapsedView = (TextView) this.findViewById(R.id.timeElapsed);
+        CountDownTimer = new MyCountDownTimer(startTime, interval);
+        text.setText(text.getText() + String.valueOf(startTime));
     }
 }
